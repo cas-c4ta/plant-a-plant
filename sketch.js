4d1a28551305feb649b213c7f2e50fbecacf3bee
Coding for the Arts <88927553+cas-c4ta@users.noreply.github.com>
2023-10-09 10:24:29 +0200

    update aufgabenstellung

--- sketch.js
+++ sketch.js
@@ -16,5 +16,8 @@ function mousePressed() {
 }
 
 /* 
-Gibt es Möglichkeiten, die Pflanzen zu steuern?  (z.B. Tastendruck)
+- Schreibe ein Programm, dass auf Mausklick eine Pflanze an eine Position x/y setzt.
+- Alle Pflanzen haben die gleichen Eigenschaften, bei jeder einzelnen Pflanze sind sie aber etwas anders ausgeprägt.
+- Die Pflanze entwickelt/verändert sich. Vielleicht über einen bestimmten Zeitraum, vielleicht durch einen bestimmten Reiz (Klick, Tastendruck)
+- Finde selber passende Erweiterungen für deine Pflanze: Blüten, Früchte, Parasiten, etc.
 */
